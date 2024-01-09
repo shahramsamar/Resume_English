80fb1f61ea3b029e025a417176d18bf529afda55
shahram <shahramsamar2010@gmail.com>
2024-01-09 12:34:26 +0330

    register admin site
    create admin site and register site

--- website/models.py
+++ website/models.py
@@ -9,4 +9,5 @@ class Contact(models.Model):
         create_date =models.DateTimeField(auto_now_add =True)
         updated_date = models.DateTimeField(auto_now=True)
         
-        
+        def __str__(self):
+                return self.name
