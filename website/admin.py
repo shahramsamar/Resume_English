d0dab81c794265eda28a814ca3b64776817c87a5
shahram <shahramsamar2010@gmail.com>
2024-01-09 19:46:11 +0330

    use models in admin.site
    create admin.py in mysite

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from website.models import Contact
+
 # Register your models here.
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'receive_date'
+    empty_value_display = '-empty-'
+    list_display = ('name','subject','receive_date','response','response_date')
+    list_filter = ('response',)        
+    ordering =['receive_date']
+    search_fields =['name','subject']
+admin.site.register(Contact,ContactAdmin)
--- website/models.py
+++ website/models.py
@@ -5,8 +5,10 @@ class Contact(models.Model):
         name = models.CharField(max_length=255)
         email = models.EmailField()
         subject = models.CharField(max_length=255)
-        message =models.TextField()
-        create_date =models.DateTimeField(auto_now_add =True)
+        message = models.TextField()
+        response = models.BooleanField(default = False)
+        response_date = models.DateTimeField()
+        receive_date = models.DateTimeField(auto_now_add =True)
         updated_date = models.DateTimeField(auto_now=True)
         
         def __str__(self):
