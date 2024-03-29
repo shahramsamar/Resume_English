430ea209da89ff5d675ca44ed0ff7f1dbd5d9094
shahram <shahramsamar2010@gmail.com>
2024-03-29 13:08:27 +0330

    correcting name in project

--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# class BasicInformationModel(models.Model):
+        
 class Contact(models.Model):
         name = models.CharField(max_length=255)
         email = models.EmailField()
