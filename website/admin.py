e0169abb67879b72a4ce0355e86a207d93f8d3bf
shahram <shahramsamar2010@gmail.com>
2024-04-01 22:33:05 +0330

    added basic model

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from website.models import Contact
+from website.models import Contact, BasicInformationModel
 
 # Register your models here.
+
+class BasicInformationAdmin(admin.ModelAdmin):
+        list_display = ('id','age')
+
+    
 class ContactAdmin(admin.ModelAdmin):
     date_hierarchy = 'create_date'
     # empty_value_display = '-empty-'
@@ -11,3 +16,4 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields =['name','message']
     
 admin.site.register(Contact,ContactAdmin)
+admin.site.register(BasicInformationModel)
--- website/models.py
+++ website/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
-# class BasicInformationModel(models.Model):
+class BasicInformationModel(models.Model):
+        about = models.TextField()
+        age = models.PositiveIntegerField()
+        email = models.EmailField()
+        phone_number = models.CharField(max_length=255)
+        address = models.CharField(max_length=255)
+        language = models.CharField(max_length=255)
         
 class Contact(models.Model):
         name = models.CharField(max_length=255)
